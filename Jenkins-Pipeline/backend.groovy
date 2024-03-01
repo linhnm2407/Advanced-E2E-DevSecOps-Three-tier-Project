52083c2539681bd7b68f2812f84d6f7c0f0374a5
linhnm2407 <linhnm2407@gmail.com>
2024-03-01 10:52:47 +0700

    Add Jenkins pipeline backend

--- Jenkins-Pipeline/backend.groovy
+++ Jenkins-Pipeline/backend.groovy
@@ -27,6 +27,17 @@ pipeline {
 
          stage('Sonarqube-Analysis') {
             steps {
+                dir('Application-Code/backend') {
+                    withSonarQubeEnv('sonar-server') {
+                        sh '''
+                        $SCANNER_HOME/bin/sonar-scanner \
+                        -Dsonar.projectKey=three-tie-backend \
+                        -Dsonar.sources=. \
+                        -Dsonar.host.url=https://app-sonarqube.linhnm.com \
+                        -Dsonar.login=squ_772cca21a9cd0cec861379e1fedc786218661158
+                        '''
+                    }
+                }
                 
             }
          }
